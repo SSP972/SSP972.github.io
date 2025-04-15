9e6b6a6df1d9baf2285dddb97e79a8306ca80fe6
ssp972 <65234695+SSP972@users.noreply.github.com>
2025-04-15 18:40:48 +0530

    Update port.js

--- port.js
+++ port.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Business Analyst"],
+        strings: ["Data Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Business Analyst","Data Science Enthusiast"],
+        strings: ["Data Analyst","Data Science Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
